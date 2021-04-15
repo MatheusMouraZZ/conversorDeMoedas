f6a08f90f851216bde4ac040719a7bbb117a524a
Matheus Moura <68867385+MatheusMouraZZ@users.noreply.github.com>
2021-04-15 13:15:23 -0300

    Add files via upload

--- Projeto pessoal/script.js
+++ Projeto pessoal/script.js
@@ -10,4 +10,45 @@ input.addEventListener('keydown', function(e){
     if(keyPressed == 8) { //se o backspace for pressionado, acontecerá:
         e.target.value = e.target.value.slice(0, -1) //apagar o último caractere do campo
     }
+    if(keyPressed == 48 || keyPressed == 96) { 
+        e.target.value = input.value + parseInt(keyValue)
+    }
+    converter()
 })
+var select = document.querySelector("#choose-coin");
+select.addEventListener("change", function(){
+    converter()
+})
+function converter(){
+    var coinValue, prefix, resultado = input.value
+    var resultadoFinal = document.querySelector("#converted-coin")
+    var unHide = document.querySelector(".result")
+    switch(select.value){
+        case "NOK":
+            coinValue = 1.49
+            prefix = "kr"
+            break
+        case "LIB":
+            coinValue = 0.13
+            prefix = "£"
+            break
+        case "USD":
+            coinValue = 0.18
+            prefix = "$"
+            break
+        case "JPY":
+            coinValue = 19.28
+            prefix = "¥"
+            break
+        case "EUR":
+            coinValue = 0.15
+            prefix = "€"
+            break
+        default: break
+    }
+    resultado = resultado * coinValue
+    resultado = resultado.toFixed(2);
+    resultado = prefix + resultado
+    resultadoFinal.innerHTML = resultado
+    unHide.classList.remove("hide")
+}
